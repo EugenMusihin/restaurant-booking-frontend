9066105e94f05b9ec168cb07914303a008227de9
musikhineg <egmusikhin@edu.hse.ru>
2025-03-20 15:43:11 +0500

    final commit

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,9 @@
 import axios from "axios";
-// import Cookies from "js-cookie";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000",  // Базовый URL бекенда
-    withCredentials: true,  // Если используется авторизация с cookies
+    baseURL: "http://127.0.0.1:8000",
+    withCredentials: true,
 });
-
-// API.interceptors.request.use(config => {
-//     const token = Cookies.get('access_token'); // Get token from cookies
-//     console.log(token);
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//
-//     return config;
-// }, error => Promise.reject(error));
-
-
 
 export async function getUserProfile() {
     try {
